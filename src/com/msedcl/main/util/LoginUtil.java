af9537a2974e1bd0d5741b67721a293e4e79a35f
student5 <student5@host.docker.internal>
2026-05-25 14:22:31 +0530

    Final Commit

--- src/com/msedcl/main/util/LoginUtil.java
+++ src/com/msedcl/main/util/LoginUtil.java
@@ -2,6 +2,7 @@ package com.msedcl.main.util;
 
 public class LoginUtil {
 	public boolean validateUserName(String userName) {
+		System.out.println("Validation");
 		if (userName.equals("admin"))
 			return true;
 		return false;
